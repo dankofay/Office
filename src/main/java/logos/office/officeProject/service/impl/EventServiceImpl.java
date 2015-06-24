2aa7577bbc6025f665de0b210dc642d0060fd704
lembergIP <User@192.168.0.102>
2015-06-24 08:39:32 +0300

    n

--- src/main/java/logos/office/officeProject/service/impl/EventServiceImpl.java
+++ src/main/java/logos/office/officeProject/service/impl/EventServiceImpl.java
@@ -19,7 +19,6 @@ import logos.office.officeProject.model.Type;
 import logos.office.officeProject.model.User;
 import logos.office.officeProject.service.EventService;
 
-
 @Service
 public class EventServiceImpl implements EventService {
 	@Inject
@@ -27,36 +26,35 @@ public class EventServiceImpl implements EventService {
 
 	@Inject
 	private TypeDao typeDao;
-	
+
 	@Inject
 	private ScheduleDao scheduleDao;
-	
+
 	@Transactional
-	public List<EventDTO> getAllEventsByIdUser(String email){
-		List<EventDTO>edto=new ArrayList<>();
-		UserServiceImpl us=new UserServiceImpl();
-		Long id_user=us.idUserByEmail(email);
+	public List<EventDTO> getAllEventsByIdUser(String email) {
+		List<EventDTO> edto = new ArrayList<>();
+		UserServiceImpl us = new UserServiceImpl();
+		Long id_user = us.idUserByEmail(email);
 		for (Event ek : eventDao.getAllElements()) {
-			Iterator<User>iter=ek.getUsers().iterator();
-			while(iter.hasNext()){
-				User i=iter.next();
-				if(i.getId()==id_user){
-					edto.add(new EventDTO(ek.getType().getNameType(), ek.getSchedule().getDate(),
-							ek.getTimeFrom(), ek.getDuration());
+			Iterator<User> iter = ek.getUsers().iterator();
+			while (iter.hasNext()) {
+				User i = iter.next();
+				if (i.getId() == id_user) {
+					edto.add(new EventDTO(ek.getType().getNameType(), ek
+							.getSchedule().getDate(), ek.getTimeFrom(), ek
+							.getDuration()));
 				}
 			}
-		}
-		
-		
+		}return edto;
+
 	}
-	
+
 	@Transactional
 	public List<Event> getAllEvents() {
 		return eventDao.getAllElements();
 
 	}
-	
-	
+
 	@Transactional
 	public List<Event> findEventsByTypeName(String typeName) {
 		return eventDao.findEventsByTypeName(typeName);
@@ -70,8 +68,14 @@ public class EventServiceImpl implements EventService {
 
 	@Transactional
 	public List<Event> findEventsByTypeDate(Date date) {
-		
+
 		return eventDao.findEventsByTypeDate(date);
+	}
+
+	@Override
+	public List<EventDTO> getAllEventsByIdUser(Long id_user) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
