46bb3479425e8a9893211f67c4ad8cc8f87219cc
Longer <Longer@komp14>
2015-06-29 18:26:13 +0300

    edit service

--- src/main/java/logos/office/officeProject/service/impl/NewsServiceImpl.java
+++ src/main/java/logos/office/officeProject/service/impl/NewsServiceImpl.java
@@ -2,9 +2,12 @@ package logos.office.officeProject.service.impl;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import javax.inject.Inject;
 import javax.transaction.Transactional;
+
 import org.springframework.stereotype.Service;
+
 import logos.office.officeProject.dao.NewsDAO;
 import logos.office.officeProject.dto.NewsDTO;
 import logos.office.officeProject.model.News;
@@ -15,21 +18,29 @@ public class NewsServiceImpl implements NewsService{
     @Inject
     NewsDAO newsdao;
 
-    @Transactional
-    public List<NewsDTO> getAllNewsById(Long id){
-    	List<NewsDTO> ndto = new ArrayList<NewsDTO>();
-    	
-    	for (News news : newsdao.getAllElements());
+//    @Transactional
+//    public List<NewsDTO> getAllNewsByIdUser(Long id){
+//    	List<NewsDTO> ndto = new ArrayList<>();
+//    	
+//    	for (News news : newsdao.getAllElements()) {
+//    		List<News> ln = news.getUser().getNews();
+//    		Long ln = news.getUser().getId();
+//    		if (ln.equals(id)){
+//    			id = getAllNews();
+//    			return id;
+//    	    if (news.getId() == id){
+//    		  ndto.add(new NewsDTO(news.getTitle(),
+//    				  news.getComment(), news.getRating()));
     		
-    	
-    	
-		return ndto;
-    	
-    	
+//    		}
+//    	}
+//    	  return ndto;
+//    }
+    
+    @Transactional
+    public void News (){
     	
     }
-    
-    
     
     
     
